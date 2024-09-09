37975e6d4c151dfb4444e8575ad1ea26f5159309
yuki <92524570+yuk1c@users.noreply.github.com>
2024-09-09 22:04:04 +0500

    Config update.
    Updated config with minor fixes. Some of the unused variables were removed. More variables will be added soon.
    
    Signed-off-by: yuki <92524570+yuk1c@users.noreply.github.com>

--- config.sh
+++ config.sh
@@ -3,10 +3,10 @@
 #################
 
 # Binary routes.
+nft="/sbin/nft"
 IP="/sbin/iptables-nft"
 IP6="/sbin/ip6tables-nft"
-nft="/sbin/nft"
-SC="/sbin/sysctl"
+sc_reload="/sbin/sysctl -p"
 
 # For working with sysctl.conf
 sysctl_conf="/etc/sysctl.conf"
@@ -16,15 +16,16 @@ max_backups=5
 timestamp=$(date "+%Y%m%d%H%M%S")
 backup_file="$backup_dir/sysctl.conf_$timestamp"
 
-# SSH server Port.
-# Ignore this if you don't use/don't have an SSH server.
+# SSH server port. (TCP only)
+# You can comment it if you don't have SSH.
 # Default: 22 (ssh)
 SSH="22"
+# SSH="2222" # For port 2222 (commonly used)
 
 # All ports of running webservers.
 # Ignore this if you don't have them.
 # If you wanna add a port, it should look like "80,443,8080".
-# Default: 80
+# Default: 80 (http)
 HTTP="80"
 
 # Connection limit
@@ -55,39 +56,11 @@ UPL="3000/second"
 # Default: 2/second
 IPL="2/second"
 
-# Hashtable size (buckets)
-# Default: 65536
-HTS="65536"
-
-# Hashtable max entries in the hash
-# Default: 65536
-HTM="65536"
-
-# Hashtable expire (ms)
-# Default: 5 minutes (300000 ms)
-HTE="300000"
-
-# MSS limit
-# Default: 536:65535
-MSS="536:65535"
-
 # Packet state filter
-ST="invalid,untracked"
+ST="invalid, untracked"
 
 # Limited UDP source ports (against amplification)
 LUSP="19, 53, 111, 123, 137, 389, 1900, 3702, 5353"
-
-# Invalid TCP Flag packet action
-# Default: DROP
-ITFPA="DROP"
-
-# Outgoing port-unreach limit
-# Default: 5/m
-OPL="5/m"
-
-# Outgoing TCP RST limit
-# Default: 10/s
-OTRL="10/s"
 
 # Colors
 BGray="\033[1;37m"
